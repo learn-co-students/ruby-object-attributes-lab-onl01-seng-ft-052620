3a0a536a7cbe58559a411961d8463274c5d63e9f
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-03 00:47:48 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(name0)
+    @name = name0
+  end
+  
+  def name
+    @name
+  end 
+  
+  
+  def job=(ocupation)
+    @job = ocupation
+  end
+  
+  def job
+    @job
+  end
+end
