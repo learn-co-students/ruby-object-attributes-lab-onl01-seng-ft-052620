99b9200943d7326ec8b6b333acd6d6a45a3acd3b
Zoe Keller <keller.zoe98@gmail.com>
2020-06-10 14:08:23 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(current_name)
+    @name= current_name
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def job=(type)
+    @job= type 
+  end 
+  
+  def job
+    @job 
+  end 
+  
+end 
